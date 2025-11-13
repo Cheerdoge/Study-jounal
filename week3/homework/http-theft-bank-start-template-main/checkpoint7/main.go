195bdb97e924c2afab7763279c5f1ab3791d0100
Cheerdoge <3218296001@qq.com>
2025-11-13 13:20:21 +0800

    <add>this week's homework

---
+++ week3/homework/http-theft-bank-start-template-main/checkpoint7/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/weiji6/hacker-support/httptool"
+)
+
+func main() {
+	req, err := httptool.NewRequest(
+		httptool.PATCHMETHOD,
+		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/muxi/backend/computer/examination",
+		"D:\\code\\study-jounal\\week3\\homework\\num.go",
+		httptool.FILE, // 这里可能不是 DEFAULT，自己去翻阅文档
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req.AddHeader("passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiQ2hlZXJkb2dlIiwiaWF0IjoxNzYyODY4NjE2LCJuYmYiOjE3NjI4Njg2MTZ9.siKywu0RdUEwq5vwB33ijTkeO3B3Rj5bm7ZfIZx5Y8E")
+
+	resp, err := req.SendRequest()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	resp.ShowHeader()
+	resp.ShowBody()
+}
--- week3/homework/num.go
+++ week3/homework/num.go
@@ -4,19 +4,33 @@ import "fmt"
 
 func permute(nums []int) [][]int {
 	// insert your code
-
+	result := make([][]int, 0)
+	var dowork func(int)
+	dowork = func(count int) {
+		if count == len(nums) {
+			value := make([]int, len(nums))
+			copy(value, nums)
+			result = append(result, value)
+		}
+		for i := count; i < len(nums); i++ {
+			nums[count], nums[i] = nums[i], nums[count]
+			dowork(count + 1)
+			nums[count], nums[i] = nums[i], nums[count]
+		}
+	}
+	dowork(0)
+	return result
 }
 
 func main() {
 	var n int
-	fmt.Scanf("%d", &n)
+	fmt.Scanf("%d\n", &n)
 
 	testSlice := make([]int, n)
 	// 标准输入n个不重复的数字
 	for i := 0; i < n; i++ {
 		fmt.Scanf("%d", &testSlice[i])
 	}
-
 	res := permute(testSlice)
 	fmt.Println(res)
 }
