a946367f6f0421d7214d8f9cd7a465b1ab4b1f45
Cheerdoge <3218296001@qq.com>
2025-11-13 21:09:43 +0800

    <fix>add a new channel to control the out

--- week2/homework/channel-control/omg.go
+++ week2/homework/channel-control/omg.go
@@ -8,28 +8,29 @@ import (
 )
 
 func main() {
-	lingpai := make(chan struct{}, 1)
+	lingpai1 := make(chan struct{}, 1)
+	lingpai2 := make(chan struct{}, 1)
 
-	lingpai <- struct{}{}
+	lingpai1 <- struct{}{}
 
 	go func() {
 		s := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 		for i := 0; i < 24; i++ {
-			<-lingpai
+			<-lingpai1
 			fmt.Printf("%c", s[i])
 			i++
 			fmt.Printf("%c", s[i])
-			lingpai <- struct{}{}
+			lingpai2 <- struct{}{}
 		}
 	}()
 
 	go func() {
 		for i := 0; i < 26; i++ {
-			<-lingpai
+			<-lingpai2
 			fmt.Printf("%d", i)
 			i++
 			fmt.Printf("%d", i)
-			lingpai <- struct{}{}
+			lingpai1 <- struct{}{}
 		}
 	}()
 
